a822cdf93374a9574b1680a4fa065ce0910d3f84
shima8823 <taiyaki1275@gmail.com>
2024-04-27 23:19:49 +0900

    feat: function calculating

--- calculator_proj/lib/main.dart
+++ calculator_proj/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:calculator_proj/numpad.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +10,221 @@ void main() {
 
 class MainAppState extends ChangeNotifier {
   String equation = "0";
-  String result = "0";
+  String resultText = "0";
+  double result = 0.0;
+
+  bool _isFirstInput() {
+    return equation == "0";
+  }
+
+  bool _isPointAlready(String text, String equation) {
+    return (text == "." && equation.contains("."));
+  }
+
+  bool _includeOperator() {
+    return equation.contains("+") ||
+        equation.contains("-") ||
+        equation.contains("×") ||
+        equation.contains("/");
+  }
+
+  int _getLastOperatorIndex() {
+    // equationの最後から走査して最初に+ or - or x or /が見つかったindexを取得
+    int operatorIndex = equation.lastIndexOf("+");
+    operatorIndex =
+        operatorIndex == -1 ? equation.lastIndexOf("-") : operatorIndex;
+    operatorIndex =
+        operatorIndex == -1 ? equation.lastIndexOf("×") : operatorIndex;
+    operatorIndex =
+        operatorIndex == -1 ? equation.lastIndexOf("/") : operatorIndex;
+    return operatorIndex;
+  }
+
+  void initialize() {
+    equation = "0";
+    resultText = "0";
+    result = 0.0;
+    notifyListeners();
+  }
+
+  int _skipExponentIndexOf(String s) {
+    // +の前にeがある場合、+は演算子ではないのでスキップ
+
+    for (int i = 0; i < equation.length; i++) {
+      if (equation[i] == s) {
+        if (i == 0 || equation[i - 1] != "e") {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
+
+  void calculateEquation() {
+    bool endWithOperator = equation.endsWith("+") ||
+        equation.endsWith("-") ||
+        equation.endsWith("×") ||
+        equation.endsWith("/");
+
+    if (equation.length > 20) {
+      equation = "0";
+      resultText = "Error";
+      result = 0.0;
+      return;
+    }
+
+    // 電卓アルゴリズム 乗算・除算を優先して計算
+    // 1. 乗算・除算のindexを取得
+    // 2. 乗算・除算のindexが見つかった場合、その前後の数値を計算して置換
+    // 3. 乗算・除算のindexが見つからなくなるまで繰り返す
+    // 4. 足し算・引き算を計算
+    String equationtemp = equation;
+    while (equation.contains("×") || equation.contains("/")) {
+      int index = equation.contains("×") == false
+          ? equation.indexOf("/")
+          : equation.indexOf("×");
+      int leftIndex = index - 1;
+      int rightIndex = index + 1;
+      while (leftIndex > 0 &&
+          equation[leftIndex] != "+" &&
+          equation[leftIndex] != "-") {
+        leftIndex--;
+      }
+      while (rightIndex < equation.length &&
+          equation[rightIndex] != "+" &&
+          equation[rightIndex] != "-") {
+        rightIndex++;
+      }
+      double left = double.parse(equation.substring(leftIndex + 1, index));
+      double right = double.parse(equation.substring(index + 1, rightIndex));
+      double result = equation[index] == "×" ? left * right : left / right;
+      equation = equation.substring(0, leftIndex + 1) +
+          result.toString() +
+          equation.substring(rightIndex);
+    }
+    int plusIndex = _skipExponentIndexOf("+");
+    int minusIndex = _skipExponentIndexOf("-");
+    while (plusIndex != -1 || minusIndex != -1) {
+      int index;
+      if (plusIndex == -1) {
+        index = minusIndex;
+      } else if (minusIndex == -1) {
+        index = plusIndex;
+      } else {
+        index = min(plusIndex, minusIndex);
+      }
+
+      int leftIndex = index - 1;
+      int rightIndex = index + 1;
+      while (leftIndex > 0 &&
+          equation[leftIndex] != "+" &&
+          equation[leftIndex] != "-") {
+        // +の前にeがある場合、+は演算子ではないのでスキップ
+        leftIndex--;
+      }
+      while (rightIndex < equation.length &&
+          equation[rightIndex] != "+" &&
+          equation[rightIndex] != "-") {
+        rightIndex++;
+      }
+      if (leftIndex == 0) {
+        leftIndex--;
+      }
+
+      double left = double.parse(equation.substring(leftIndex + 1, index));
+
+      double right = double.parse(equation.substring(index + 1, rightIndex));
+      double result = equation[index] == "+" ? left + right : left - right;
+
+      equation = equation.substring(0, leftIndex + 1) +
+          result.toString() +
+          equation.substring(rightIndex);
+      plusIndex = _skipExponentIndexOf("+");
+      minusIndex = _skipExponentIndexOf("-");
+    }
+    // .00の場合、.を削除
+    if (equation.endsWith(".0")) {
+      equation = equation.substring(0, equation.length - 2);
+    }
+    resultText = equation;
+    equation = equationtemp;
+
+    if (equation == "Infinity") {
+      resultText = "0";
+      result = 0.0;
+      return;
+    }
+    result = double.parse(resultText);
+  }
+
+  void numClick(String text) {
+    // validate point
+    int operatorIndex = _getLastOperatorIndex();
+    if (operatorIndex == -1) {
+      if (_isPointAlready(text, equation)) {
+        return;
+      }
+    } else {
+      // operatorIndexの次の文字から最後までを取得
+      String lastInput = equation.substring(operatorIndex + 1);
+      if (_isPointAlready(text, lastInput)) {
+        return;
+      }
+    }
+
+    // validate first input
+    if (_isFirstInput()) {
+      if (text == "00" || text == ".") {
+        return;
+      }
+      equation = text;
+      result = double.parse(text);
+      notifyListeners();
+      return;
+    }
+    equation += text;
+    calculateEquation();
+    notifyListeners();
+  }
+
+  void operatorClick(String text) {
+    switch (text) {
+      case "+":
+      case "-":
+      case "×":
+      case "/":
+        if (_isFirstInput()) {
+          return;
+        }
+        if (equation.endsWith("+") ||
+            equation.endsWith("-") ||
+            equation.endsWith("×") ||
+            equation.endsWith("/")) {
+          equation = equation.substring(0, equation.length - 1) + text;
+          break;
+        }
+        equation += text;
+        break;
+      case "=":
+        if (_isFirstInput() ||
+            equation.endsWith("+") ||
+            equation.endsWith("-") ||
+            equation.endsWith("×") ||
+            equation.endsWith("/")) {
+          return;
+        }
+        equation = result.toString();
+        resultText = "0";
+        break;
+      case "C":
+        equation = "0";
+        break;
+      case "AC":
+        initialize();
+        break;
+    }
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -51,7 +267,7 @@ class Calculator extends StatelessWidget {
                   Text(appState.equation,
                       style: TextStyle(
                           color: Colors.indigo[200], fontSize: textSize)),
-                  Text(appState.result,
+                  Text(appState.resultText,
                       style: TextStyle(
                           color: Colors.indigo[200], fontSize: textSize)),
                 ],
--- calculator_proj/lib/numpad.dart
+++ calculator_proj/lib/numpad.dart
@@ -1,161 +1,9 @@
+import 'package:calculator_proj/main.dart';
 import 'package:flutter/material.dart';
-
-class CalculatorButton {
-  final String text;
-  final Color textColor;
-  final VoidCallback onPressed;
-
-  CalculatorButton({
-    required this.text,
-    required this.textColor,
-    required this.onPressed,
-  });
-}
+import 'package:provider/provider.dart';
+import 'button.dart';
 
 Widget numpad(BuildContext context) {
-  final buttons = [
-    CalculatorButton(
-      text: '7',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :7');
-      },
-    ),
-    CalculatorButton(
-      text: '8',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :8');
-      },
-    ),
-    CalculatorButton(
-      text: '9',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :9');
-      },
-    ),
-    CalculatorButton(
-      text: 'C',
-      textColor: Colors.red,
-      onPressed: () {
-        print('button pressed :C');
-      },
-    ),
-    CalculatorButton(
-      text: 'AC',
-      textColor: Colors.red,
-      onPressed: () {
-        print('button pressed :AC');
-      },
-    ),
-    CalculatorButton(
-      text: '4',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :4');
-      },
-    ),
-    CalculatorButton(
-      text: '5',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :5');
-      },
-    ),
-    CalculatorButton(
-      text: '6',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :6');
-      },
-    ),
-    CalculatorButton(
-      text: '+',
-      textColor: Colors.white,
-      onPressed: () {
-        print('button pressed :+');
-      },
-    ),
-    // - minus button
-    CalculatorButton(
-      text: '-',
-      textColor: Colors.white,
-      onPressed: () {
-        print('button pressed :-');
-      },
-    ),
-    CalculatorButton(
-      text: '1',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :1');
-      },
-    ),
-    CalculatorButton(
-      text: '2',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :2');
-      },
-    ),
-    CalculatorButton(
-      text: '3',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :3');
-      },
-    ),
-    CalculatorButton(
-      text: '×',
-      textColor: Colors.white,
-      onPressed: () {
-        print('button pressed :×');
-      },
-    ),
-    CalculatorButton(
-      text: '/',
-      textColor: Colors.white,
-      onPressed: () {
-        print('button pressed :/');
-      },
-    ),
-    CalculatorButton(
-      text: '0',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :0');
-      },
-    ),
-    CalculatorButton(
-      text: '.',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :.');
-      },
-    ),
-    CalculatorButton(
-      text: '00',
-      textColor: Colors.black,
-      onPressed: () {
-        print('button pressed :00');
-      },
-    ),
-    CalculatorButton(
-      text: '=',
-      textColor: Colors.white,
-      onPressed: () {
-        print('button pressed :=');
-      },
-    ),
-    // change to Container
-    CalculatorButton(
-      text: '',
-      textColor: Colors.white,
-      onPressed: () {},
-    ),
-  ];
-
   return GridView.builder(
     shrinkWrap: true,
     itemCount: buttons.length,
@@ -171,20 +19,33 @@ Widget numpad(BuildContext context) {
           color: Colors.indigo[300],
         );
       }
-      return calcButton(
-          context,
-          buttons[index].text,
-          buttons[index].textColor,
-          () => {
-                // appState.addNumber(buttons[index].text),
-                print('button pressed : ${buttons[index].text}')
-              });
+      return calcButton(context, buttons[index].text, buttons[index].textColor,
+          buttons[index].buttonType);
     },
   );
 }
 
 Widget calcButton(BuildContext context, String buttonText, Color buttonColor,
-    void Function()? buttonPressed) {
+    ButtonType buttonType) {
+  final appState = context.watch<MainAppState>();
+  void buttonPressed() {
+    try {
+      switch (buttonType) {
+        case ButtonType.num:
+          appState.numClick(buttonText);
+          break;
+        case ButtonType.operator:
+          appState.operatorClick(buttonText);
+          break;
+      }
+    } catch (e) {
+      appState.initialize();
+      appState.resultText = "Format Error";
+    }
+  }
+
+  ;
+
   return Center(
       child: RawMaterialButton(
     onPressed: buttonPressed,
