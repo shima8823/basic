0df6beb30aaf70da4c71442601b40653ad0500fb
shima8823 <taiyaki1275@gmail.com>
2024-04-28 12:53:59 +0900

    fix: add minus button and format expression

--- calculator_proj/lib/main.dart
+++ calculator_proj/lib/main.dart
@@ -30,13 +30,12 @@ class MainAppState extends ChangeNotifier {
 
   int _getLastOperatorIndex() {
     // equationの最後から走査して最初に+ or - or x or /が見つかったindexを取得
-    int operatorIndex = equation.lastIndexOf("+");
-    operatorIndex =
-        operatorIndex == -1 ? equation.lastIndexOf("-") : operatorIndex;
-    operatorIndex =
-        operatorIndex == -1 ? equation.lastIndexOf("×") : operatorIndex;
-    operatorIndex =
-        operatorIndex == -1 ? equation.lastIndexOf("/") : operatorIndex;
+    int plusIndex = equation.lastIndexOf("+");
+    int minusIndex = equation.lastIndexOf("-");
+    int multiplyIndex = equation.lastIndexOf("×");
+    int divideIndex = equation.lastIndexOf("/");
+    int operatorIndex =
+        max(plusIndex, max(minusIndex, max(multiplyIndex, divideIndex)));
     return operatorIndex;
   }
 
@@ -48,11 +47,16 @@ class MainAppState extends ChangeNotifier {
   }
 
   int _skipExponentIndexOf(String s) {
-    // +の前にeがある場合、+は演算子ではないのでスキップ
+    // skip "e+", "e-", "+-", "--", "x-", "/-"
 
     for (int i = 0; i < equation.length; i++) {
       if (equation[i] == s) {
-        if (i == 0 || equation[i - 1] != "e") {
+        if (i > 0 &&
+            (equation[i - 1] != "e" &&
+                equation[i - 1] != "+" &&
+                equation[i - 1] != "-" &&
+                equation[i - 1] != "×" &&
+                equation[i - 1] != "/")) {
           return i;
         }
       }
@@ -61,11 +65,7 @@ class MainAppState extends ChangeNotifier {
   }
 
   void calculateEquation() {
-    bool endWithOperator = equation.endsWith("+") ||
-        equation.endsWith("-") ||
-        equation.endsWith("×") ||
-        equation.endsWith("/");
-
+    print("first equation: $equation");
     if (equation.length > 20) {
       equation = "0";
       resultText = "Error";
@@ -80,21 +80,47 @@ class MainAppState extends ChangeNotifier {
     // 4. 足し算・引き算を計算
     String equationtemp = equation;
     while (equation.contains("×") || equation.contains("/")) {
-      int index = equation.contains("×") == false
-          ? equation.indexOf("/")
-          : equation.indexOf("×");
+      int multiplyIndex = equation.indexOf("×");
+      int divideIndex = equation.indexOf("/");
+      int index;
+      if (multiplyIndex == -1) {
+        index = divideIndex;
+      } else if (divideIndex == -1) {
+        index = multiplyIndex;
+      } else {
+        index = min(multiplyIndex, divideIndex);
+      }
+
       int leftIndex = index - 1;
       int rightIndex = index + 1;
       while (leftIndex > 0 &&
           equation[leftIndex] != "+" &&
-          equation[leftIndex] != "-") {
+          (equation[leftIndex] != "-" &&
+              (equation[leftIndex - 1] != "e" ||
+                  equation[leftIndex - 1] != "+" ||
+                  equation[leftIndex - 1] != "×" ||
+                  equation[leftIndex - 1] != "/"))) {
         leftIndex--;
       }
       while (rightIndex < equation.length &&
-          equation[rightIndex] != "+" &&
-          equation[rightIndex] != "-") {
+          (equation[rightIndex] != "+" &&
+              (equation[rightIndex] != "-" ||
+                  (equation[rightIndex] == "-" &&
+                      (equation[rightIndex - 1] == "e" ||
+                          equation[rightIndex - 1] == "+" ||
+                          equation[rightIndex - 1] == "-" ||
+                          equation[rightIndex - 1] == "×" ||
+                          equation[rightIndex - 1] ==
+                              "/"))) // -の後ろにoperatorがある場合はスキップ
+              &&
+              equation[rightIndex] != "×" &&
+              equation[rightIndex] != "/")) {
         rightIndex++;
       }
+      if (leftIndex == 0) {
+        leftIndex--;
+      }
+
       double left = double.parse(equation.substring(leftIndex + 1, index));
       double right = double.parse(equation.substring(index + 1, rightIndex));
       double result = equation[index] == "×" ? left * right : left / right;
@@ -102,6 +128,8 @@ class MainAppState extends ChangeNotifier {
           result.toString() +
           equation.substring(rightIndex);
     }
+
+    print("second equation: $equation");
     int plusIndex = _skipExponentIndexOf("+");
     int minusIndex = _skipExponentIndexOf("-");
     while (plusIndex != -1 || minusIndex != -1) {
@@ -118,13 +146,27 @@ class MainAppState extends ChangeNotifier {
       int rightIndex = index + 1;
       while (leftIndex > 0 &&
           equation[leftIndex] != "+" &&
-          equation[leftIndex] != "-") {
+          (equation[leftIndex] != "-" &&
+              (equation[leftIndex - 1] != "e" ||
+                  equation[leftIndex - 1] != "+" ||
+                  equation[leftIndex - 1] != "×" ||
+                  equation[leftIndex - 1] != "/"))) {
         // +の前にeがある場合、+は演算子ではないのでスキップ
         leftIndex--;
       }
       while (rightIndex < equation.length &&
-          equation[rightIndex] != "+" &&
-          equation[rightIndex] != "-") {
+          (equation[rightIndex] != "+" &&
+              (equation[rightIndex] != "-" ||
+                  (equation[rightIndex] == "-" &&
+                      (equation[rightIndex - 1] == "e" ||
+                          equation[rightIndex - 1] == "+" ||
+                          equation[rightIndex - 1] == "-" ||
+                          equation[rightIndex - 1] == "×" ||
+                          equation[rightIndex - 1] ==
+                              "/") // -の後ろにoperatorがある場合はスキップ
+                  )) &&
+              equation[rightIndex] != "×" &&
+              equation[rightIndex] != "/")) {
         rightIndex++;
       }
       if (leftIndex == 0) {
@@ -136,6 +178,10 @@ class MainAppState extends ChangeNotifier {
       double right = double.parse(equation.substring(index + 1, rightIndex));
       double result = equation[index] == "+" ? left + right : left - right;
 
+      print("equation: $equation");
+      print(equation.substring(0, leftIndex + 1));
+      print(result.toString());
+      print(equation.substring(rightIndex));
       equation = equation.substring(0, leftIndex + 1) +
           result.toString() +
           equation.substring(rightIndex);
@@ -149,7 +195,7 @@ class MainAppState extends ChangeNotifier {
     resultText = equation;
     equation = equationtemp;
 
-    if (equation == "Infinity") {
+    if (equation.contains("Infinity")) {
       resultText = "0";
       result = 0.0;
       return;
@@ -174,7 +220,13 @@ class MainAppState extends ChangeNotifier {
 
     // validate first input
     if (_isFirstInput()) {
-      if (text == "00" || text == ".") {
+      if (text == "00") {
+        return;
+      }
+      if (text == ".") {
+        equation = "0.";
+        resultText = "0.";
+        notifyListeners();
         return;
       }
       equation = text;
@@ -183,7 +235,9 @@ class MainAppState extends ChangeNotifier {
       return;
     }
     equation += text;
-    calculateEquation();
+    if (_includeOperator()) {
+      calculateEquation();
+    }
     notifyListeners();
   }
 
@@ -193,17 +247,32 @@ class MainAppState extends ChangeNotifier {
       case "-":
       case "×":
       case "/":
-        if (_isFirstInput()) {
+        bool isMinus = text == "-";
+        if (!isMinus && _isFirstInput()) {
           return;
         }
-        if (equation.endsWith("+") ||
+        if (isMinus) {
+          if (equation.endsWith("-")) {
+            return;
+          }
+          if (equation.endsWith("+") ||
+              equation.endsWith("×") ||
+              equation.endsWith("/")) {
+            equation = equation + text;
+            break;
+          }
+        } else if (equation.endsWith("+") ||
             equation.endsWith("-") ||
             equation.endsWith("×") ||
             equation.endsWith("/")) {
           equation = equation.substring(0, equation.length - 1) + text;
           break;
         }
-        equation += text;
+        if (isMinus && _isFirstInput()) {
+          equation = text;
+        } else {
+          equation += text;
+        }
         break;
       case "=":
         if (_isFirstInput() ||
@@ -214,10 +283,13 @@ class MainAppState extends ChangeNotifier {
           return;
         }
         equation = result.toString();
+        if (equation.endsWith(".0")) {
+          equation = equation.substring(0, equation.length - 2);
+        }
         resultText = "0";
         break;
       case "C":
-        equation = "0";
+        equation = resultText;
         break;
       case "AC":
         initialize();
@@ -234,7 +306,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MainAppState(),
-      child: MaterialApp(
+      child: const MaterialApp(
         title: 'Calculator',
         home: Calculator(),
       ),
@@ -277,3 +349,25 @@ class Calculator extends StatelessWidget {
             ]));
   }
 }
+
+
+// test
+// 1 + 2 * 3 - 5 / 2 = 4.5
+// 1 + 2
+// 10 - 5
+// 3.5 * 4
+// 20 / 4
+// 1 / 0 = Infinity
+// -10 + 5
+// 3 * (-4)
+// 2 + 3 * 4 - 6 / 3
+// 5.2 + -3.7 * 2.1        = -2.57
+// -10 / 2 + 3.5 * -4      = -19
+// 100.5 - 25.25 * 2 + 6.5 = 56.5
+// -0.2 * -0.4 / 5 + 3.1   = 3.116
+// 15 / 3 - 2.5 * -1.2     = 8
+// -6.6 + 2.2 * 3 - 4 / -2 = 2 NG
+// 50 / -5 + 7.7 - 4.4     = -6.7
+// -12.5 * 0.8 + 6 / 3     = -8
+// 25.25 / 5 - 3.3 * 1.5   = 0.1
+// -0.75 * -20 + 8 / 4     = 17
